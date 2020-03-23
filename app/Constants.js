abbdd75f4f32ba01934fcbe414213e7669fc029c
Aki Haimi <aki.haimi@haimisolutions.com>
2020-03-23 20:58:50 +0200

    Add TTFI & domInteractive.

--- app/Constants.js
+++ app/Constants.js
@@ -4,148 +4,166 @@ export default {
       html: {
         bytes: {
           color: [115, 210, 222],
-          name: 'HTML',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "HTML",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [115, 210, 222],
-          name: 'HTML'
+          name: "HTML"
         }
       },
       js: {
         bytes: {
           color: [251, 177, 60],
-          name: 'JS',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "JS",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [251, 177, 60],
-          name: 'JS'
+          name: "JS"
         }
       },
       css: {
         bytes: {
           color: [33, 131, 128],
-          name: 'CSS',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "CSS",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [33, 131, 128],
-          name: 'CSS'
+          name: "CSS"
         }
       },
       image: {
         bytes: {
           color: [143, 45, 86],
-          name: 'Images',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "Images",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [143, 45, 86],
-          name: 'Images'
+          name: "Images"
         }
       },
       flash: {
         bytes: {
           color: [33, 41, 92],
-          name: 'Flash',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "Flash",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [33, 41, 92],
-          name: 'Flash'
+          name: "Flash"
         }
       },
       font: {
         bytes: {
           color: [216, 17, 89],
-          name: 'Fonts',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "Fonts",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [216, 17, 89],
-          name: 'Fonts'
+          name: "Fonts"
         }
       },
       other: {
         bytes: {
           color: [231, 143, 142],
-          name: 'Other',
-          transform: (value) => (value / 1000).toFixed(1),
-          unit: 'KB'
+          name: "Other",
+          transform: value => (value / 1000).toFixed(1),
+          unit: "KB"
         },
         requests: {
           color: [231, 143, 142],
-          name: 'Other'
+          name: "Other"
         }
       }
     },
     loadTime: {
       color: [195, 125, 146],
-      name: 'Load time',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'The time between the initial request and the browser load event'
+      name: "Load time",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time between the initial request and the browser load event"
     },
     firstPaint: {
       color: [173, 138, 100],
-      name: 'Start render',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'The time until the browser starts painting content to the screen'
+      name: "Start render",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time until the browser starts painting content to the screen"
     },
     fullyLoaded: {
       color: [3, 181, 170],
-      name: 'Fully loaded',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'The time at which the page has fully finished loading content'
+      name: "Fully loaded",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time at which the page has fully finished loading content"
     },
     SpeedIndex: {
       color: [87, 117, 144],
-      name: 'SpeedIndex',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'A custom metric introduced by WebPageTest to rate pages based on how quickly they are visually populated'
+      name: "SpeedIndex",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "A custom metric introduced by WebPageTest to rate pages based on how quickly they are visually populated"
     },
     TTFB: {
       color: [80, 48, 71],
-      name: 'Back-end',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'The time it takes for the server to respond with the first byte of the response'
+      name: "Back-end (TTFB)",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time it takes for the server to respond with the first byte of the response"
+    },
+    TTFI: {
+      color: [247, 81, 61],
+      name: "First Interactive",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "This is a newer metric (TTFI) and reports when the page is first expected to be usable and will respond to input quickly (with the possibility of slow responses as more content loads)."
     },
     visualComplete: {
       color: [243, 202, 64],
-      name: 'Visually complete',
-      transform: (value) => (value / 1000).toFixed(2),
-      unit: 's',
-      description: 'The time it takes for the page to be fully visually populated'
+      name: "Visually complete",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time it takes for the page to be fully visually populated"
+    },
+    domInteractive: {
+      color: [243, 202, 64],
+      name: "DOM Interactive",
+      transform: value => (value / 1000).toFixed(2),
+      unit: "s",
+      description:
+        "The time it takes for the page to be fully visually populated"
     },
     pagespeed: {
       color: [51, 103, 214],
-      name: 'PageSpeed score',
-      unit: '',
-      description: 'A custom metric defined by Google PageSpeed Insights'
+      name: "PageSpeed score",
+      unit: "",
+      description: "A custom metric defined by Google PageSpeed Insights"
     },
     lighthouse: {
       color: [121, 180, 115],
-      name: 'Lighthouse PWA score',
-      unit: '',
-      description: 'A Progressive Web App (PWA) score defined by Google Lighthouse'
+      name: "Lighthouse PWA score",
+      unit: "",
+      description:
+        "A Progressive Web App (PWA) score defined by Google Lighthouse"
     }
   },
 
-  periods: [
-    'day',
-    'week',
-    'month',
-    'year'
-  ]
-}
+  periods: ["day", "week", "month", "year"]
+};
