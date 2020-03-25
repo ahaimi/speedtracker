23b0670fe343a5b82559199f3a1bcd59e8304383
Aki Haimi <aki.haimi@haimisolutions.com>
2020-03-25 15:09:53 +0200

    Update network timing metrics colors.

--- app/Constants.js
+++ app/Constants.js
@@ -178,27 +178,27 @@ export default {
       description: "The time it takes to perform the DNS lookup."
     },
     connect_ms: {
-      color: [251, 177, 60],
+      color: [143, 45, 86],
       name: "Initial connection",
       unit: "ms",
       description:
         "Time it took to establish a connection, including TCP handshakes/retries and negotiating a SSL."
     },
     ssl_ms: {
-      color: [143, 45, 86],
+      color: [251, 177, 60],
       name: "SSL handshake",
       unit: "ms",
       description: "Time spent completing a SSL handshake."
     },
     ttfb_ms: {
-      color: [33, 41, 92],
+      color: [115, 210, 222],
       name: "Server response",
       unit: "ms",
       description:
         "Waiting (TTFB), latency of a round trip to the server in addition to the time spent waiting for the server to deliver the response."
     },
     download_ms: {
-      color: [115, 210, 222],
+      color: [33, 41, 92],
       name: "Content download",
       unit: "ms",
       description: "Time spent receiving the response data."
